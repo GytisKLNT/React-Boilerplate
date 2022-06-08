df7e6f76bbe4638d4f4da8f9e5d54d55785e24af
GytisKlnt <gytis.kalanta@gmail.com>
2022-06-08 12:22:45 +0300

    added styled components

---
+++ src/components/Button/Button.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background: transparent;
+  cursor: pointer;
+  border: 0.05 solid black;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+`;
