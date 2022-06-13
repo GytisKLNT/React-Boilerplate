9c2d1a941c2ca205c37ed7feaccfe8c9e1a3a6bc
GytisKlnt <gytis.kalanta@gmail.com>
2022-06-13 19:34:15 +0300

    added routes

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
